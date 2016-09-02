6746164f308faf5c0cfe5d68ef4955ff027ab4ee
Chris Vig <christopher.vig@garmin.com>
2016-09-02 11:52:17 -0500

    Add Swift project.

---
+++ examples/Tao/Tao/Tao.h
@@ -0,0 +1,19 @@
+//
+//  Tao.h
+//  Tao
+//
+//  Created by Chris Vig on 9/2/16.
+//  Copyright © 2016 Invictus. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Tao.
+FOUNDATION_EXPORT double TaoVersionNumber;
+
+//! Project version string for Tao.
+FOUNDATION_EXPORT const unsigned char TaoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Tao/PublicHeader.h>
+
+
