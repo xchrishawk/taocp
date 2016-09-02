b8ea9e6d2fabd6365c9869a1422fc21ae8a2434c
Chris Vig <chris@invictus.so>
2016-09-02 14:26:11 -0500

    Initial implementation for CUnit.

--- examples/c/main.c
+++ examples/c/main.c
@@ -1,6 +1,17 @@
-#include <stdio.h>
+/* -- Includes -- */
+
+#include <stdlib.h>
+#include <CUnit/CUnit.h>
+#include <CUnit/Basic.h>
+
+/* -- Procedures -- */
 
 int main(int argc, char** argv)
 {
-  return 0;
+  CU_initialize_registry();
+  CU_basic_set_mode(CU_BRM_VERBOSE);
+  CU_basic_run_tests();
+  CU_cleanup_registry();
+
+  return (CU_get_number_of_failures() != 0 ? EXIT_FAILURE : EXIT_SUCCESS);
 }
